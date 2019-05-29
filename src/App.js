45250738a7833166ce2ec7d57f2c62309fde712f
Roberto <robpalacios1@hotmail.com>
2019-05-29 15:40:48 -0500

    show a list

--- src/App.js
+++ src/App.js
@@ -9,12 +9,15 @@ class App extends Component {
       list: ['Javascript', 'CSS', 'HTLM', 'React']
     }
   }
-  render() {
+  render() { 
+    const list = ['Javascript', 'CSS', 'HTLM', 'React']
     return (
       <div style={styles.container}>
         <h1 style={styles.title}>Las tecnologias del Front</h1>
         <ul style={styles.list}>
-          {/* Tu codigo va aca */}
+          {list.map(list =>
+            <li>{list}</li>
+          )}
         </ul>
       </div>
     );
